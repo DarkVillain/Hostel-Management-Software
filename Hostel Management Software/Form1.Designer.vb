bdba0c7e99258dcd54b00e535863960c9552d6ba
dark <dark@DESKTOP-EG48M20>
2020-03-16 02:16:05 +0530

    Login Page Done

--- Hostel Management Software/Form1.Designer.vb
+++ Hostel Management Software/Form1.Designer.vb
@@ -26,12 +26,18 @@ Partial Class Login
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Top = New System.Windows.Forms.Panel()
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.pic = New System.Windows.Forms.PictureBox()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.lblUname = New System.Windows.Forms.Label()
+        Me.lblPass = New System.Windows.Forms.Label()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.btnClr = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.llFP = New System.Windows.Forms.LinkLabel()
+        Me.llCP = New System.Windows.Forms.LinkLabel()
+        Me.lblTitle = New System.Windows.Forms.Label()
         Me.Top.SuspendLayout()
         Me.Panel1.SuspendLayout()
-        CType(Me.pic, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
@@ -47,18 +53,19 @@ Partial Class Login
         'Label2
         '
         Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.White
         Me.Label2.Font = New System.Drawing.Font("Microsoft Uighur", 20.25!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.ForeColor = System.Drawing.Color.White
-        Me.Label2.Location = New System.Drawing.Point(327, 13)
+        Me.Label2.ForeColor = System.Drawing.Color.DarkViolet
+        Me.Label2.Location = New System.Drawing.Point(124, 12)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(68, 36)
+        Me.Label2.Size = New System.Drawing.Size(94, 36)
         Me.Label2.TabIndex = 2
-        Me.Label2.Text = "Sign In"
+        Me.Label2.Text = "< Sign In >"
         '
         'Top
         '
         Me.Top.BackColor = System.Drawing.Color.DarkViolet
-        Me.Top.Controls.Add(Me.Label2)
+        Me.Top.Controls.Add(Me.lblTitle)
         Me.Top.Controls.Add(Me.Label1)
         Me.Top.Dock = System.Windows.Forms.DockStyle.Top
         Me.Top.Location = New System.Drawing.Point(0, 0)
@@ -68,37 +75,128 @@ Partial Class Login
         '
         'Panel1
         '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.llCP)
+        Me.Panel1.Controls.Add(Me.llFP)
+        Me.Panel1.Controls.Add(Me.btnExit)
+        Me.Panel1.Controls.Add(Me.btnClr)
+        Me.Panel1.Controls.Add(Me.btnLogin)
+        Me.Panel1.Controls.Add(Me.lblPass)
+        Me.Panel1.Controls.Add(Me.lblUname)
         Me.Panel1.Controls.Add(Me.TextBox2)
         Me.Panel1.Controls.Add(Me.TextBox1)
-        Me.Panel1.Location = New System.Drawing.Point(235, 178)
+        Me.Panel1.Location = New System.Drawing.Point(202, 181)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(262, 290)
+        Me.Panel1.Size = New System.Drawing.Size(332, 335)
         Me.Panel1.TabIndex = 3
-        '
-        'pic
-        '
-        Me.pic.BackColor = System.Drawing.Color.Transparent
-        Me.pic.Image = Global.Hostel_Management_Software.My.Resources.Resources.PicsArt_03_16_01_05_41
-        Me.pic.Location = New System.Drawing.Point(313, 115)
-        Me.pic.Name = "pic"
-        Me.pic.Size = New System.Drawing.Size(100, 97)
-        Me.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.pic.TabIndex = 4
-        Me.pic.TabStop = False
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(27, 79)
+        Me.TextBox1.BackColor = System.Drawing.Color.White
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Location = New System.Drawing.Point(49, 122)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(202, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(236, 20)
         Me.TextBox1.TabIndex = 0
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(27, 130)
+        Me.TextBox2.BackColor = System.Drawing.Color.White
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox2.Location = New System.Drawing.Point(49, 183)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(202, 20)
+        Me.TextBox2.Size = New System.Drawing.Size(236, 20)
         Me.TextBox2.TabIndex = 1
+        '
+        'lblUname
+        '
+        Me.lblUname.AutoSize = True
+        Me.lblUname.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblUname.ForeColor = System.Drawing.Color.DarkViolet
+        Me.lblUname.Location = New System.Drawing.Point(47, 93)
+        Me.lblUname.Name = "lblUname"
+        Me.lblUname.Size = New System.Drawing.Size(77, 16)
+        Me.lblUname.TabIndex = 2
+        Me.lblUname.Text = "User Name"
+        '
+        'lblPass
+        '
+        Me.lblPass.AutoSize = True
+        Me.lblPass.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPass.ForeColor = System.Drawing.Color.DarkViolet
+        Me.lblPass.Location = New System.Drawing.Point(46, 155)
+        Me.lblPass.Name = "lblPass"
+        Me.lblPass.Size = New System.Drawing.Size(68, 16)
+        Me.lblPass.TabIndex = 3
+        Me.lblPass.Text = "Password"
+        '
+        'btnLogin
+        '
+        Me.btnLogin.BackColor = System.Drawing.Color.Green
+        Me.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnLogin.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnLogin.Location = New System.Drawing.Point(49, 231)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(75, 23)
+        Me.btnLogin.TabIndex = 4
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = False
+        '
+        'btnClr
+        '
+        Me.btnClr.BackColor = System.Drawing.Color.Gold
+        Me.btnClr.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnClr.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.btnClr.Location = New System.Drawing.Point(130, 231)
+        Me.btnClr.Name = "btnClr"
+        Me.btnClr.Size = New System.Drawing.Size(75, 23)
+        Me.btnClr.TabIndex = 5
+        Me.btnClr.Text = "Clear"
+        Me.btnClr.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.Red
+        Me.btnExit.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnExit.ForeColor = System.Drawing.Color.White
+        Me.btnExit.Location = New System.Drawing.Point(211, 231)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'llFP
+        '
+        Me.llFP.AutoSize = True
+        Me.llFP.Location = New System.Drawing.Point(48, 284)
+        Me.llFP.Name = "llFP"
+        Me.llFP.Size = New System.Drawing.Size(92, 13)
+        Me.llFP.TabIndex = 7
+        Me.llFP.TabStop = True
+        Me.llFP.Text = "Forgot Password?"
+        '
+        'llCP
+        '
+        Me.llCP.AutoSize = True
+        Me.llCP.Location = New System.Drawing.Point(193, 284)
+        Me.llCP.Name = "llCP"
+        Me.llCP.Size = New System.Drawing.Size(93, 13)
+        Me.llCP.TabIndex = 8
+        Me.llCP.TabStop = True
+        Me.llCP.Text = "Change Password"
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("MingLiU_HKSCS-ExtB", 24.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.ForeColor = System.Drawing.Color.White
+        Me.lblTitle.Location = New System.Drawing.Point(145, 58)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(423, 32)
+        Me.lblTitle.TabIndex = 2
+        Me.lblTitle.Text = "Sonar Bangla Boys Hostel"
         '
         'Login
         '
@@ -106,7 +204,6 @@ Partial Class Login
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(715, 592)
-        Me.Controls.Add(Me.pic)
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.Top)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
@@ -116,7 +213,6 @@ Partial Class Login
         Me.Top.PerformLayout()
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
-        CType(Me.pic, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -125,7 +221,14 @@ Partial Class Login
     Friend WithEvents Label2 As Label
     Friend WithEvents Top As Panel
     Friend WithEvents Panel1 As Panel
-    Friend WithEvents pic As PictureBox
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents lblUname As Label
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents llCP As LinkLabel
+    Friend WithEvents llFP As LinkLabel
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnClr As Button
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents lblPass As Label
 End Class
--- Hostel Management Software/Form1.vb
+++ Hostel Management Software/Form1.vb
@@ -2,4 +2,8 @@
     Private Sub Panel2_Paint(sender As Object, e As PaintEventArgs)
 
     End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles lblUname.Click
+
+    End Sub
 End Class
