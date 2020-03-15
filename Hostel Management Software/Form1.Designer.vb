91e5f173fbbc305dddaa1c3130a23cf02be70594
dark <dark@DESKTOP-EG48M20>
2020-03-15 11:31:53 +0530

    Form border style changed

--- Hostel Management Software/Form1.Designer.vb
+++ Hostel Management Software/Form1.Designer.vb
@@ -29,6 +29,7 @@ Partial Class Form1
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(715, 450)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
