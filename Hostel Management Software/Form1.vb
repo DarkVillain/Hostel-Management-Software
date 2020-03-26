37558d5f976a586170da5dbfa182e67834dd4a29
Subhajit Sarkar <get2subhajit@hotmail.com>
2020-03-26 23:34:14 +0530

    will add user icon later

--- Hostel Management Software/Form1.vb
+++ Hostel Management Software/Form1.vb
@@ -17,7 +17,11 @@
 
     End Sub
 
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub PictureBox1_Click_1(sender As Object, e As EventArgs)
 
     End Sub
 End Class
