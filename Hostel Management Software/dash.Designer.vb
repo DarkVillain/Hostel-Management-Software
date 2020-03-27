1e077afb8cf690b30c1a374ef56e6a5b34a9d9b4
Subhajit Sarkar <get2subhajit@hotmail.com>
2020-03-27 23:40:06 +0530

    Designed tab menus for Dashboard

--- Hostel Management Software/dash.Designer.vb
+++ Hostel Management Software/dash.Designer.vb
@@ -22,7 +22,96 @@ Partial Class dash
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.AlloteesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HostelToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RoomToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.KitchenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RentToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PaymentToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ReportToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AddToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DeleteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AlloteesToolStripMenuItem, Me.HostelToolStripMenuItem, Me.RoomToolStripMenuItem, Me.KitchenToolStripMenuItem, Me.RentToolStripMenuItem, Me.PaymentToolStripMenuItem, Me.ReportToolStripMenuItem, Me.AboutToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'AlloteesToolStripMenuItem
+        '
+        Me.AlloteesToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AddToolStripMenuItem, Me.EditToolStripMenuItem, Me.DeleteToolStripMenuItem})
+        Me.AlloteesToolStripMenuItem.Name = "AlloteesToolStripMenuItem"
+        Me.AlloteesToolStripMenuItem.Size = New System.Drawing.Size(61, 20)
+        Me.AlloteesToolStripMenuItem.Text = "Allotees"
+        '
+        'HostelToolStripMenuItem
+        '
+        Me.HostelToolStripMenuItem.Name = "HostelToolStripMenuItem"
+        Me.HostelToolStripMenuItem.Size = New System.Drawing.Size(53, 20)
+        Me.HostelToolStripMenuItem.Text = "Hostel"
+        '
+        'RoomToolStripMenuItem
+        '
+        Me.RoomToolStripMenuItem.Name = "RoomToolStripMenuItem"
+        Me.RoomToolStripMenuItem.Size = New System.Drawing.Size(51, 20)
+        Me.RoomToolStripMenuItem.Text = "Room"
+        '
+        'KitchenToolStripMenuItem
+        '
+        Me.KitchenToolStripMenuItem.Name = "KitchenToolStripMenuItem"
+        Me.KitchenToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.KitchenToolStripMenuItem.Text = "Beds"
+        '
+        'RentToolStripMenuItem
+        '
+        Me.RentToolStripMenuItem.Name = "RentToolStripMenuItem"
+        Me.RentToolStripMenuItem.Size = New System.Drawing.Size(43, 20)
+        Me.RentToolStripMenuItem.Text = "Rent"
+        '
+        'PaymentToolStripMenuItem
+        '
+        Me.PaymentToolStripMenuItem.Name = "PaymentToolStripMenuItem"
+        Me.PaymentToolStripMenuItem.Size = New System.Drawing.Size(66, 20)
+        Me.PaymentToolStripMenuItem.Text = "Payment"
+        '
+        'ReportToolStripMenuItem
+        '
+        Me.ReportToolStripMenuItem.Name = "ReportToolStripMenuItem"
+        Me.ReportToolStripMenuItem.Size = New System.Drawing.Size(54, 20)
+        Me.ReportToolStripMenuItem.Text = "Report"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(52, 20)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'AddToolStripMenuItem
+        '
+        Me.AddToolStripMenuItem.Name = "AddToolStripMenuItem"
+        Me.AddToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.AddToolStripMenuItem.Text = "Add"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.EditToolStripMenuItem.Text = "Edit"
+        '
+        'DeleteToolStripMenuItem
+        '
+        Me.DeleteToolStripMenuItem.Name = "DeleteToolStripMenuItem"
+        Me.DeleteToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.DeleteToolStripMenuItem.Text = "Delete"
         '
         'dash
         '
@@ -31,9 +120,27 @@ Partial Class dash
         Me.BackgroundImage = Global.Hostel_Management_Software.My.Resources.Resources.hostelbg
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "dash"
         Me.Text = "Welcome to Dashboard!!"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents AlloteesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AddToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DeleteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HostelToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RoomToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents KitchenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RentToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PaymentToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ReportToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
 End Class
